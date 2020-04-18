b85d5859d0bf0e8c47f4dcfde5672246fc4f888c
Kai Marshall <k.marshall@setonhill.edu>
2020-04-17 20:43:31 -0400

    Ready to grade

--- Homework 9/ViewController.swift
+++ Homework 9/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBAction func openSite(_ sender: Any) {
+        
+        if let url = URL(string: "https://campustours.com/college/215947/Seton-Hill-University/") {
+            UIApplication.shared.open(url, options: [:])
+        }
+        
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //webView
+        let myURL = URL(string:"https://campustours.com/college/215947/Seton-Hill-University/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+      
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
 }
+
 
