98a501c63a00ffbea594b47b26bf713c13a20928
Kai Marshall <k.marshall@setonhill.edu>
2020-04-17 21:24:33 -0400

    Ready to grade

--- Homework 9/ViewController.swift
+++ Homework 9/ViewController.swift
@@ -8,14 +8,46 @@
 
 import UIKit
 import WebKit
-class ViewController: UIViewController {
+import MessageUI
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        
+    }
+    
 
     @IBOutlet weak var webView: WKWebView!
     @IBAction func openSite(_ sender: Any) {
+       
+        func messageComposeViewController(_ controller: MFMessageComposeViewController, didFininshWith result: MessageComposeResult) {
+            
+            
+        }
+     
+        
+        
         
         if let url = URL(string: "https://campustours.com/college/215947/Seton-Hill-University/") {
             UIApplication.shared.open(url, options: [:])
         }
+        
+        
+        
+    }
+    @IBAction func sendSMS(_ sender: Any) {
+        let composeVC = MFMessageComposeViewController()
+        composeVC.messageComposeDelegate = self
+        
+        composeVC.recipients = ["8134516746"]
+        composeVC.body = "Hello, This message is being sent from my app."
+        
+        if MFMessageComposeViewController.canSendText()
+        {
+            self.present(composeVC,animated: true, completion: nil)
+        } else {
+            print("Can't send messages.")
+        }
+        
+        
         
         
         
@@ -27,6 +59,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //webView
+        
+        
         let myURL = URL(string:"https://campustours.com/college/215947/Seton-Hill-University/")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
